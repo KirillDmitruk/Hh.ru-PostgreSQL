08d4145003b72604c7961e36083ba8209ab75cd6
Kirill Dmitruk <dmitruk.ks@mail.ru>
2024-03-15 11:54:51 +0300

    Received a list of sortable employers and a list of sortable vacancies

---
+++ src/Hh_API.py
@@ -0,0 +1,131 @@
+import json
+import time
+from abc import ABC, abstractmethod
+import requests
+
+
+class AbstractAPI(ABC):
+    """
+    Абстрактный класс с методом получения данных по API
+    """
+    @abstractmethod
+    def get_employers(self):
+        pass
+
+
+class HeadHunterData(AbstractAPI):
+    """
+    Класс получения данных по API
+    """
+    def __init__(self):
+        self.employers = []
+        self.all_vacancies = []
+        # self.companies = ['ITFB Group', 'Caltat', 'ООО Гексагон', 'ООО Экспомобилити', 'Яндекс']
+        self.sort_emp = []
+        self.sort_vac = []
+
+    def get_employers(self):
+        """
+        Метод позволяет получить данные по определенным параметрам с сайта HH.ru
+        :return: self.employers
+        """
+
+        params = {
+            'area': 113,
+            'only_with_vacancies': True,
+            'per_page': 10
+        }
+
+        hh_url = 'https://api.hh.ru/employers'
+        data = requests.get(hh_url, params=params).json()
+
+        self.employers.extend(data['items'])
+
+        return self.employers
+
+    def get_sort_emp(self):
+        """
+        Метод добавляет компании из списка self.employers по заданным критериям в список self.sort_emp
+        :return: self.sort_emp
+        """
+        self.sort_emp.clear()
+        for emp in self.employers:
+            emp_dict = {
+                'id': f'{emp["id"]}',
+                'name': f'{emp["name"]}',
+                'url': f'{emp["alternate_url"]}',
+                'vacancies_url': f"https://api.hh.ru/vacancies?employer_id={emp['id']}",
+                'open_vacancies': emp["open_vacancies"]
+            }
+
+            self.sort_emp.append(emp_dict)
+
+        return self.sort_emp
+
+    def get_vacancies_from_employer(self):
+        """
+        Метод позволяет получить данные по определенным параметрам с сайта HH.ru
+        :return:
+        """
+        for emp in self.sort_emp:
+            hh_url = f'{emp["vacancies_url"]}'
+            response = requests.get(hh_url).json()
+
+            self.all_vacancies.extend(response['items'])
+
+        return self.all_vacancies
+
+    def get_sort_vacancies(self):
+        """
+        Метод получения отсортированного списка
+        :return:
+        """
+        for vac in self.all_vacancies:
+
+            id_emp = f'{vac["id"]}'
+            name = f'{vac["name"]}'
+            area = f'{vac["area"]["name"]}'
+            # currency = f'{vac["salary"]["currency"]}'
+            employer_id = f'{vac["employer"]["id"]}'
+            employer_name = f'{vac["employer"]["name"]}'
+            emp_url = f'{vac["employer"]["alternate_url"]}'
+            vac_url = f'{vac["alternate_url"]}'
+
+            if vac["salary"] is not None:
+                if vac["salary"]["from"] is not None:
+
+                    salary_from = vac["salary"]["from"]
+                else:
+                    salary_from = 0
+
+                if vac["salary"]["to"] is not None:
+
+                    salary_to = vac["salary"]["to"]
+                else:
+                    salary_to = 0
+            else:
+                salary_from, salary_to = 0, 0
+
+            vac_dict = {
+                'id': id_emp,
+                'name_vac': name,
+                'area': area,
+                'salary_from': salary_from,
+                'salary_to': salary_to,
+                # 'currency': currency,
+                'employer_id': employer_id,
+                'employer_name': employer_name,
+                'employer_url': emp_url,
+                'vacancy_url': vac_url
+            }
+
+            self.sort_vac.append(vac_dict)
+        return self.sort_vac
+
+
+if __name__ == '__main__':
+    x = HeadHunterData()
+    x.get_employers()
+    print(x.get_sort_emp())
+    x.get_vacancies_from_employer()
+    # print(x.get_sort_vacancies())
